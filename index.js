c1b2b624cad53a6ad3ce4a39e84e898e287471d5
stelleal <stellalleal@gmail.com>
2022-05-04 18:32:25 -0300

    Parameterized URLs

--- index.js
+++ index.js
@@ -4,20 +4,40 @@ const PORT = 3000;
 
 const server = http.createServer();
 
+const friends = [
+	{
+		id: 1,
+		name: 'Midori',
+	},
+	{
+		id: 2,
+		name: 'Gato',
+	},
+	{
+		id: 3,
+		name: 'Fifi',
+	},
+];
+
 server.on('request', (req, res) => {
-	if (req.url === '/friends') {
+	const items = req.url.split('/');
+	// /friends/2 => ['', 'friends', '2']
+
+	if (items[1] === 'friends') {
 		// res.writeHead(200, {
 		// 	'Content-Type': 'application/json',
 		// });
 		res.statusCode = 200;
 		res.setHeader('Content-Type', 'application/json');
-		res.end(
-			JSON.stringify({
-				id: 1,
-				name: 'Midori',
-			})
-		);
-	} else if (req.url === '/messages') {
+
+		if (items.length === 3) {
+			const friendIndex = --items[2];
+			res.end(JSON.stringify(friends[friendIndex]));
+		} else {
+			res.end(JSON.stringify(friends));
+		}
+
+	} else if (items[1] === 'messages') {
 		res.setHeader('Content-Type', 'text/html');
 		res.write('<html>');
 		res.write('<body>');
@@ -28,10 +48,11 @@ server.on('request', (req, res) => {
 		res.write('</body>');
 		res.write('</html>');
 		res.end();
+
 	} else {
-    res.statusCode = 404;
-    res.end();
-  }
+		res.statusCode = 404;
+		res.end();
+	}
 });
 
 server.listen(PORT, () => {
