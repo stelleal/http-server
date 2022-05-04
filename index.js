11076f1ee2ff4a4e2d6405f8cfa580baeb7e98c8
stelleal <stellalleal@gmail.com>
2022-05-04 16:23:11 -0300

    Basic JSON server starter

---
+++ index.js
@@ -0,0 +1,19 @@
+const http = require('http');
+
+const PORT = 3000;
+
+const server = http.createServer((req, res) => {
+	res.writeHead(200, {
+		'Content-Type': 'application/json',
+	});
+	res.end(
+		JSON.stringify({
+			id: 1,
+			name: 'Midori',
+		})
+	);
+});
+
+server.listen(PORT, () => {
+	console.log(`Listening on port ${PORT}...`);
+});
